83fab5db0b9d630948a51cc52c59f94c18ae5b6c
Kevin Kang <kevink97@cs.washington.edu>
2019-03-25 18:14:41 -0700

    Added images for website

---
+++ src/components/images/W.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.app/gatsby-image
+ * - `StaticQuery`: https://gatsby.app/staticquery
+ */
+
+const CourtImage = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "W.jpg" }) {
+          childImageSharp {
+            fixed(height: 2366, cropFocus: NORTHWEST) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Img
+        className="courtPic"
+        imgStyle={{ objectPosition: "0 0" }}
+        style={{ width: "100%" }}
+        fixed={data.placeholderImage.childImageSharp.fixed}
+      />
+    )}
+  />
+);
+
+export default CourtImage;
---
+++ src/components/images/nationals.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `StaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.app/gatsby-image
+ * - `StaticQuery`: https://gatsby.app/staticquery
+ */
+
+const NationalsImage = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "nationals.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 436) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Img
+        fluid={data.placeholderImage.childImageSharp.fluid}
+        style={{ boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.25)" }}
+      />
+    )}
+  />
+);
+
+export default NationalsImage;
