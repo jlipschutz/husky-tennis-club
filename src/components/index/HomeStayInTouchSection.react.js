5c06bcc09ccceeb2c0d700e37ed3f3c36b9e70a8
Kevin Kang <kevink97@cs.washington.edu>
2019-03-29 01:06:08 -0700

    fixed minor bug

--- src/components/index/HomeStayInTouchSection.react.js
+++ src/components/index/HomeStayInTouchSection.react.js
@@ -45,46 +45,44 @@ class HomeStayInTouchSection extends React.Component<Props> {
             </div>
           </div>
           <div className="row d-flex justify-content-center">
-            <div className="row d-flex justify-content-center">
-              <div className={"col-12 col-sm-5 d-flex justify-content-center"}>
-                <GenericGradientButton
-                  site="https://mailman13.u.washington.edu/mailman/listinfo/huskytennis"
-                  width={205}
-                >
-                  <span
-                    style={{
-                      display: "inline-block",
-                      fontSize: 16,
-                      marginRight: 18
-                    }}
-                  >
-                    <FaBell />
-                  </span>
-                  SUBSCRIBE TO EMAIL
-                </GenericGradientButton>
-              </div>
-              <div className={"d-none d-sm-block col-sm-2"} />
-              <div
-                className={
-                  "col-12 col-sm-5  d-flex justify-content-center mt-sm-0 mt-3"
-                }
+            <div className={"col-12 col-sm-5 d-flex justify-content-center"}>
+              <GenericGradientButton
+                site="https://mailman13.u.washington.edu/mailman/listinfo/huskytennis"
+                width={205}
               >
-                <GenericGradientButton
-                  site="https://www.facebook.com/groups/huskytennisclub/"
-                  width={205}
+                <span
+                  style={{
+                    display: "inline-block",
+                    fontSize: 16,
+                    marginRight: 18
+                  }}
                 >
-                  <span
-                    style={{
-                      display: "inline-block",
-                      fontSize: 16,
-                      marginRight: 18
-                    }}
-                  >
-                    <FaFacebookSquare />
-                  </span>
-                  FACEBOOK GROUP
-                </GenericGradientButton>
-              </div>
+                  <FaBell />
+                </span>
+                SUBSCRIBE TO EMAIL
+              </GenericGradientButton>
+            </div>
+            <div className={"d-none d-sm-block col-sm-2"} />
+            <div
+              className={
+                "col-12 col-sm-5  d-flex justify-content-center mt-sm-0 mt-3"
+              }
+            >
+              <GenericGradientButton
+                site="https://www.facebook.com/groups/huskytennisclub/"
+                width={205}
+              >
+                <span
+                  style={{
+                    display: "inline-block",
+                    fontSize: 16,
+                    marginRight: 18
+                  }}
+                >
+                  <FaFacebookSquare />
+                </span>
+                FACEBOOK GROUP
+              </GenericGradientButton>
             </div>
           </div>
         </div>
