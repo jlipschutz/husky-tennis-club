87c1ff95c9e2239aaca895aaedfc27894c8c8489
Rishabh Goyal <rishabhgoyal555@gmail.com>
2019-04-19 17:48:23 -0700

    completed header for contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,25 +3,18 @@
  */
 import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavbar from "../components/navbar/navbar.react";
+import GenericHeader from "../components/GenericHeader.react";
 const React = require("react");
 
 type Props = {};
 
 class Contact extends React.Component<Props> {
-  getStyle = () => {
-    return {
-      marginTop: '70px',
-      marginLeft: '20px'
-    }
-  }
   render(): React.Node {
     return (
       <div>
         <CustomNavbar selected="contact" />
-        <div style={this.getStyle()}>
-          <h1> Contact Us </h1>
-        </div>
-      </div >
+        <GenericHeader>Contact Us</GenericHeader>
+      </div>
     );
   }
 }
