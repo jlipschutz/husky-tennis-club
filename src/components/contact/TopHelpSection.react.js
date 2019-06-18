4c71180aaeb8c746357efbe3e827825bd027cfdd
Kevin <kevinkang06@gmail.com>
2019-06-17 22:04:09 -0700

    finished contact us page

--- src/components/contact/TopHelpSection.react.js
+++ src/components/contact/TopHelpSection.react.js
@@ -5,6 +5,8 @@
 const React = require("react");
 const GenericQAComponent = require("../contact/GenericQAComponent.react");
 const { QAData } = require("./QAData");
+const { FaFacebookSquare, FaEnvelope } = require("react-icons/fa");
+const GenericGradientButton = require("../../components/GenericGradientButton.react");
 
 type Props = {
   children?: React.Node
@@ -32,7 +34,55 @@ class TopHelpSection extends React.Component<Props> {
           >
             How can we help?
           </div>
-          <div className={"col-12 col-md-9"}>{QAs}</div>
+          <div className={"col-12 col-md-9"}>
+            {QAs}
+            <div className="" style={{ marginTop: 130 }}>
+              <div style={{ fontSize: 23, fontWeight: 300 }}>
+                Still need help?
+              </div>
+              <div style={{ fontSize: 15, marginTop: 20 }}>
+                Email us and one of the officers will answer your questions.
+              </div>
+              <div style={{ marginTop: 20 }}>
+                <GenericGradientButton
+                  site="mailto:huskytennis@gmail.com"
+                  width={205}
+                  redirect={false}
+                >
+                  <span
+                    style={{
+                      display: "inline-block",
+                      fontSize: 16,
+                      marginRight: 18
+                    }}
+                  >
+                    <FaEnvelope />
+                  </span>
+                  EMAIL
+                </GenericGradientButton>
+                <div style={{ fontSize: 15, marginTop: 30 }}>
+                  Or join our Facebook group!
+                </div>
+                <div style={{ marginTop: 20 }}>
+                  <GenericGradientButton
+                    site="https://www.facebook.com/groups/huskytennisclub/"
+                    width={205}
+                  >
+                    <span
+                      style={{
+                        display: "inline-block",
+                        fontSize: 16,
+                        marginRight: 18
+                      }}
+                    >
+                      <FaFacebookSquare />
+                    </span>
+                    FACEBOOK GROUP
+                  </GenericGradientButton>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
