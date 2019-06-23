0cfbfa00ce96fd4c370fdb88dc8fbb1b7a2decb5
Kevin <kevinkang06@gmail.com>
2019-06-23 13:46:25 -0700

    hacky footer solution

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavbar from "../components/navbar/navbar.react";
 const React = require("react");
 require("../style/contact.module.css");
+const genstyle = require("../style/generic.module.css");
 const GenericHeader = require("../components/GenericHeader.react");
 const GenericFooter = require("../components/GenericFooter.react");
 const TopHelpSection = require("../components/contact/TopHelpSection.react");
@@ -15,11 +16,12 @@ class Contact extends React.Component<Props> {
   render(): React.Node {
     return (
       <div>
-        <CustomNavbar selected="contact" />
-        <GenericHeader>Contact Us</GenericHeader>
-        <TopHelpSection />
-
-        <GenericFooter color="#144788" bottom={-700} />
+        <div className={genstyle.wrapper}>
+          <CustomNavbar selected="contact" />
+          <GenericHeader>Contact Us</GenericHeader>
+          <TopHelpSection />
+        </div>
+        <GenericFooter color="#144788" />
       </div>
     );
   }
