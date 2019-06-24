2157315612ea5ce0a43388f26c7d32e6d162e548
Kevin <kevinkang06@gmail.com>
2019-06-23 22:43:19 -0700

    finished event card

---
+++ src/components/event/EventType.js
@@ -0,0 +1,15 @@
+/*
+ * @flow strict-local
+ */
+
+export type EventType = {
+  name: string,
+  cityDesc: string,
+  registerBy: string,
+  fee: string,
+  dateMonth: number,
+  dateYear: number,
+  dateDayStart: number,
+  dateDayEnd: number,
+  isTravel: boolean
+};
--- src/pages/events.js
+++ src/pages/events.js
@@ -6,6 +6,7 @@ import CustomNavbar from "../components/navbar/navbar.react";
 const React = require("react");
 require("../style/event.module.css");
 const GenericHeader = require("../components/GenericHeader.react");
+const EventCard = require("../components/event/EventCard.react");
 
 type Props = {};
 
@@ -15,6 +16,19 @@ class Events extends React.Component<Props> {
       <div>
         <CustomNavbar selected="events" />
         <GenericHeader>Events</GenericHeader>
+        <EventCard
+          data={{
+            name: "2019 PNW Tennis on Campus Alumni Cup",
+            cityDesc: "University of Washington",
+            registerBy: "3/30/2019",
+            fee: "Free",
+            dateMonth: 4,
+            dateYear: 2019,
+            dateDayStart: 27,
+            dateDayEnd: 29,
+            isTravel: true
+          }}
+        />
       </div>
     );
   }
